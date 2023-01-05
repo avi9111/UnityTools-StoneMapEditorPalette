c05076e0e9f4d38ab2b15c9864c3ca759a9650b7
DESKTOP-DEBHPTM\ll <avi9111@163.com>
2023-01-05 13:23:41 +0800

    11

---
+++ SMapController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 参考2个插件：
+/// https://www.youtube.com/watch?v=ZRji3P9HKdg；
+/// https://www.synnaxium.com/en/2019/01/unity-custom-map-editor-part-1/；
+/// 想做一个快速场景构建器，目标是丝滑的移动：
+/// 
+/// </summary>
+public class SMapController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
